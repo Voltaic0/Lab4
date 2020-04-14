6009b8f8f00757376b0c6c95844a9d88d978f5d7
adecandi <adecandi@purdue.edu>
2020-04-14 05:34:13 -0400

    test3

--- one-level/apps/example/test2/test2.c
+++ one-level/apps/example/test2/test2.c
@@ -14,7 +14,7 @@ void main (int argc, char *argv[])
 
   // Convert the command-line strings into integers for use as handles
   s_procs_completed = dstrtol(argv[1], NULL, 10);
-  invalidAddress = MEM_MAX_VIRTUAL_ADDRESS + 1;
+  invalidAddress = MEM_MAX_VIRTUAL_ADDRESS - 3;
 
   // Now print a message to show that everything worked
   Printf("Attempting to access past MAX Virtual Address (%d): test2\n", getpid());
@@ -29,5 +29,5 @@ void main (int argc, char *argv[])
 
   //these should not print (unless invalid address is changed to (MEM_MAX_VIRTUAL_ADDRESS - 3))
   Printf("Accessing the value...\n %d!\n", *invalidAddress);
-  Printf("test2 (%d): Done!\n", getpid());
+  Printf("test2 (%d): Done! Address in test2 is currently a valid number.\n", getpid());
 }
